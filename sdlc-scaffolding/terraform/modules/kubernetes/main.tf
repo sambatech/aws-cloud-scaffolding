0d010e8d78469d4d5dc023211e39a62e354a6bec
Valentim <eduardo.valentim@sambatech.com.br>
2023-10-12 15:40:04 -0300

    SonarQube final setup

--- sdlc-scaffolding/terraform/modules/kubernetes/main.tf
+++ sdlc-scaffolding/terraform/modules/kubernetes/main.tf
@@ -163,6 +163,13 @@ module "eks" {
       update_config     = {
         max_unavailable = 1
       }
+      taints = [
+        {
+          key    = "dedicated"
+          value  = "sonarqube"
+          effect = "NO_SCHEDULE"
+        }
+      ]
     }
   }
 
